82952ed22c930b0c5ac0f4c5767425b829f5331c
khoahuynhdev <khoa0319@gmail.com>
2022-09-02 16:26:21 +0700

    get the proper access client key

--- index.mjs
+++ index.mjs
@@ -1,7 +1,8 @@
 import { createServer } from "http";
+import crypto from "crypto";
 
 const PORT = 1337;
-const WEB_SOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+const WEBSOCKET_MAGIC_STRING_KEY = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
 const server = createServer((req, res) => {
   res.writeHead(200);
@@ -15,6 +16,23 @@ function onSocketUpgrade(req, socket, head) {
   // Client will send the header: Sec-WebSocket-Key
   const { "sec-websocket-key": webClientSocketKey } = req.headers;
   console.log(`${webClientSocketKey} connected!`);
+  const headers = prepareHandShakeHeaders(webClientSocketKey);
+  console.log({ headers })
+}
+
+function prepareHandShakeHeaders(id) {
+  const acceptKey = createSocketAccept(id);
+  return acceptKey;
+}
+
+
+/**
+  * create sha1 + concat the id to the magic string and return base64 result
+  */
+function createSocketAccept(id) {
+  const shaum = crypto.createHash("sha1");
+  shaum.update(id + WEBSOCKET_MAGIC_STRING_KEY);
+  return shaum.digest('base64');
 }
 
 [
